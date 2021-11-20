cdd1af4ca1f83b25aa19608dfbee25faac40d637
Shahrooz Aghili <cb2aghz@ztb.icb.commerzbank.com>
2021-11-20 18:46:15 +0100

    add maria pool

--- modules/function/cf/src/index.js
+++ modules/function/cf/src/index.js
@@ -1,27 +1,24 @@
 const fastify = require('fastify')
 const app = fastify({ logger: true })
-
-const heroes = [
-  { id: 1, name: 'Iron Man' },
-  { id: 2, name: 'Thor' },
-  { id: 3, name: 'Black Widow' },
-  { id: 4, name: 'Hulk' },
-]
-
-app.get('/', async (req, res) => {
-  return { works: true }
-})
-
-app.get('/heroes', async (req, res) => {
-  return heroes
-})
-
-app.get('/heroes/:id', async (req, res) => {
-  const { id } = req.params
-  return heroes.find((h) => h.id === Number(id))
-})
-
-exports.app = async (req, res) => {
-  await app.ready()
-  app.server.emit('request', req, res)
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'my-release-mariadb',
+  user: 'bn_ghost',
+  password: 'supermaria',
+  connectionLimit: 5
+});
+async function deletePosts() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    await conn.query("DELETE FROM post");
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
 }
+app.get('/', async (req, res) => {
+  await deletePosts();
+  return { posts_deleted: true }
+})
