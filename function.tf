668cb64f3a8fa91213b0d2a35b6b789585be436f
Shahrooz Aghili <shahrooz33ce@gmail.com>
2021-11-20 16:51:17 +0000

    bugfix functions region

--- function.tf
+++ function.tf
@@ -1,6 +1,7 @@
 module "delete-posts" {
   source      = "./modules/function"
   project     = var.project_id
+  region      = var.region_primary
   name        = "delete-posts"
   entry_point = "app"
 }
